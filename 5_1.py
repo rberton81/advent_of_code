087dd5a25427797f98f8068ba410dc3ff530d3ce
Romain <romain@canold.com>
2023-12-08 13:40:51 +0700

    5_2

--- 5_1.py
+++ 5_1.py
@@ -1,4 +1,5 @@
 from utils import read_input
+import copy 
 
 def get_seeds_and_ratios(input):
     sections = [section.strip() for section in input.strip().split('\n\n')]
@@ -10,68 +11,35 @@ def get_seeds_and_ratios(input):
     for section in sections[1:]:
         lines = section.split('\n')
         key = lines[0]
-
         map_data = [[int(value) for value in line.split()] for line in lines[1:]]
-        
         ratios[key] = map_data
-
-    print("seeds:", seeds)
-    print("ratios:", ratios)
-
     return seeds, ratios
 
 def get_min_location(input):
     seeds, transaction_name__ratios = get_seeds_and_ratios(input)
-    # initial = [i for i in range(0, 100)] ##TODO only add given seeds!
-    initial = [i for i in range(0, max(seeds))]
-    # converted = [i for i in range(0, 100)]
-    converted = [i for i in range(0, max(seeds))]
+    before = {seed: seed for seed in seeds}
+    after = {seed: seed for seed in seeds}
 
-    for transaction_name, ratios in transaction_name__ratios.items():
-        print('_______________________________')
-        print(f'transaction {transaction_name}')
-
+    for _, ratios in transaction_name__ratios.items():
         for ratio in ratios:
             destination, source, _range = ratio
-            print(f"config: dest={destination} source={source} range={_range}")
 
-            for i in range(_range):
-                # print(source+i, '->', destination+i)
-                try:
-                    index = initial.index(source+i) ##TODO that wont work...
-                    converted[index] = destination+i
-                    print(f"source = {source+i}, index= {index}, destination={destination+i}")
-                    # import pdb; pdb.set_trace()
-                    # print('foo')
-                except Exception as err:
-                    print('error')
-                    import pdb; pdb.set_trace()
-                    print('error')
+            affected_range = (source, source+_range)
+            current_seeds = list(before.values())
+            affected_seeds = [seed for seed in current_seeds if affected_range[0]<=seed<affected_range[1]]
+            seed_delta = destination - source
 
+            for affected_seed in affected_seeds:
+                index = list(before.keys())[list(before.values()).index(affected_seed)]
+                after[index] += seed_delta
 
-                # converted[source+i] = destination+i
+        before = copy.deepcopy(after)
 
-            # import pdb; pdb.set_trace()
-            print('converted after')
-            for i in range(len(converted)):
-                print(f'index = {i} -> {converted[i]}')
-
-        initial = converted.copy()
-        # print(f'end transaction {transaction_name}')
-
-        # for seed in seeds:
-        #     print(f"seed {seed} -> {converted[seed]}")
-
-
-    for seed in seeds:
-        print(f"seed {seed} -> {converted[seed]}")
-
-    min_location = min([converted[seed] for seed in seeds])
-    print(f"location min : {min_location}")
+    min_location = min([after[seed] for seed in seeds])
     return min_location
 
-# example = read_input("5_example.txt", by_line=False, in_one_line=True)
-# assert(get_min_location(example) == 35)
+example = read_input("5_example.txt", by_line=False, in_one_line=True)
+assert(get_min_location(example) == 35)
 
 input = read_input("./5_input.txt", by_line=False, in_one_line=True)
 print('solution', get_min_location(input))
